8b96c22ef9f52be47a82333732409ebb87774a9e
jashelton <justin.shelton@yahoo.com>
2015-08-29 16:25:34 -0400

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function() {
+  'use strict'
+
+var quotesArr = new Array();
+
+  quotesArr[0] = 'You cant look at the competition and say youre going to do it better.  You have to look at the competition and say youre going to do it differently. - Steve Jobs';
+  quotesArr[1] = 'Its through mistakes that you actually can grow. You have to get bad in order to get good. – Paula Scher';
+  quotesArr[2] = 'Digital design is like painting, except the paint never dries. - Neville Brody'
+  quotesArr[3] = 'A designer knows he has achieved perfection not when there is nothing left to add, but when there is nothing left to take away. – Antoine de Saint-Exupéry'
+
+// $(document).load(function() {
+  var randomQuote = Math.floor(Math.random()*quotesArr.length);
+  $('.quote-content').html(quotesArr[randomQuote]);
+// });
+
+
+});
